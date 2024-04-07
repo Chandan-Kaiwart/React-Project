dcae96a2d1b313bceb2c15d52287719266c008ba
kaiwart <kaiwartji2020@gmail.com>
2024-04-07 15:40:49 +0530

    update

--- src/container/Users.js
+++ src/container/Users.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 function Users()
 {
-  const [users, setUsers] = useState([ ])
+  const [users, setUsers] = useState([ ]);
   useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => {setUsers(data)
     })
